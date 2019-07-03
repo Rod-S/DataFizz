9ee8a98e3802a72d2d2b38f7d5bea18d50c2275e
Rod S <33836692+Rod-S@users.noreply.github.com>
2019-07-03 02:27:00 -0500

    -added 4 object classes for books -(partial) conditional cases added to crawler links to decide which object class to construct

--- Crawling/crawler.js
+++ Crawling/crawler.js
@@ -2,20 +2,49 @@ const cheerio = require("cheerio");
 const site = "https://www.amazon.com";
 const fs = require("fs")
 const rp = require("request-promise");
-const request = require('request')
 
 //start message
 console.log('Executing crawler.js');
 
-class Product {
+//Define object classes - 4 main product types within Amazon book category
+class KindleBook {
+  constructor(name, listPrice, description, imageUrls) {
+    this.name = name;
+    this.listPrice = listPrice;
+    this.description = description;
+    this.imageUrls = imageUrls;
+  }
+}
+
+class AudibleBook {
+  constructor(name, listPrice, description, imageUrls) {
+    this.name = name;
+    this.listPrice = listPrice;
+    this.description = description;
+    this.imageUrls = imageUrls;
+  }
+}
+
+class HardcoverBook {
+  constructor(name, listPrice, description, imageUrls) {
+    this.name = name;
+    this.listPrice = listPrice;
+    this.description = description;
+    this.imageUrls = imageUrls;
+  }
+}
+
+class SoftcoverBook {
   constructor(name, listPrice, description, productDimensions, imageUrls, weight) {
     this.name = name;
-      this.listPrice = listPrice;
-      this.description = description;
-      this.productDimensions = productDimensions;
-      this.imageUrls = imageUrls;
-      this.weight = weight;
-  }}
+    this.listPrice = listPrice;
+    this.description = description;
+    this.productDimensions = productDimensions;
+    this.imageUrls = imageUrls;
+    this.weight = weight;
+  }
+}
+
 
 //initial rp options
 let options = {
@@ -31,6 +60,10 @@ let options = {
 rp(options).then(
    function($) {
     var products = [];
+    var kindleBooks = [];
+    var audibleBooks = [];
+    var hardcoverBooks = [];
+    var paperbackBooks = [];
     //push all product urls on page into products array
     $('.a-link-normal').each(function(i, elem){
       var hrefSubstring = $(elem).attr('href').split('/');
@@ -48,15 +81,41 @@ rp(options).then(
         gzip: true,
         transform: function (body) {
               return cheerio.load(body);
-          }
+        }
       }
       rp(options).then(
         function($) {
-            var product = new Product(
+          //if kindleBook, create kindle object
+          if ($('#ebooksProductTitle').length) {
+            var kindleBook = new KindleBook(
               $('#ebooksProductTitle').text().trim(),
-              $('.kindlePriceLabel').next().text().trim().split(' ')[0].replace('$', '')
+              $('.kindlePriceLabel').next().text().trim().split(' ')[0].replace('$', ''),
+              $('#iframeContent').text(),
+              $('#ebooks-img-canvas>img').attr('src')
             )
-            console.log(product);
-          })
-  }
+            //if audibleBook, create audible object
+          } else if ($('#productTitle').length) {
+            var audibleBook = new AudibleBook(
+              $('#productTitle').text().trim(),
+              $('.a-color-price').text().trim().split(' ')[0].replace('$', ''),
+              $('#iframeContent').text(),
+              $('#main-image').attr('src')
+            )
+          } if ($('#productTitle').next().text().includes('Hardcover')) {
+            var hardcoverBook = new HardcoverBook(
+              $('#productTitle').text().trim(),
+              $('.a-color-price').text().trim().split(' ')[0].replace('$', ''),
+              $('#iframeContent').text(),
+              $('#img-canvas>img').attr('src')
+            )
+          }
+          if(kindleBook) {
+          console.log(kindleBook);
+        } if (audibleBook) {
+          //console.log(audibleBook);
+        } if (hardcoverBook) {
+          console.log(hardcoverBook);
+        }
+      })
+    }
 });
