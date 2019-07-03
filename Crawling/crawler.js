4c53a21499427bbde21f4623c50ad3a5fce02c5d
Rod-S <33836692+Rod-S@users.noreply.github.com>
2019-07-02 21:50:28 -0500

    -changed dependency from request to request-promise module to allow promises -(partial) single generic class constructor

--- Crawling/crawler.js
+++ Crawling/crawler.js
@@ -1,28 +1,62 @@
 const cheerio = require("cheerio");
 const site = "https://www.amazon.com";
 const fs = require("fs")
-const request = require("request");
+const rp = require("request-promise");
+const request = require('request')
 
 //start message
 console.log('Executing crawler.js');
 
-//Begin scraping https://www.amazon.com/b/ref=usbk_surl_books/?node=283155, load html through cheerio
-request({
+class Product {
+  constructor(name, listPrice, description, productDimensions, imageUrls, weight) {
+    this.name = name;
+      this.listPrice = listPrice;
+      this.description = description;
+      this.productDimensions = productDimensions;
+      this.imageUrls = imageUrls;
+      this.weight = weight;
+  }}
+
+//initial rp options
+let options = {
   method: 'GET',
   uri: site + "/b/ref=usbk_surl_books/?node=283155",
-  gzip: true
-}, function(error, response, html) {
+  gzip: true,
+  transform: function (body) {
+        return cheerio.load(body);
+    }
+}
+
+//Begin scraping https://www.amazon.com/b/ref=usbk_surl_books/?node=283155, load html through cheerio
+rp(options).then(
+   function($) {
     var products = [];
-    var $ = cheerio.load(html);
     //push all product urls on page into products array
     $('.a-link-normal').each(function(i, elem){
       var hrefSubstring = $(elem).attr('href').split('/');
       if ( (hrefSubstring[1] + '/' + hrefSubstring[2]) == 'gp/product') {
-        if () {
           products.push(site + "/" + hrefSubstring[1] + '/' + hrefSubstring[2] + '/' + hrefSubstring[3])
-        }
       }
-    });
+    })
     console.log(products);
-    return products;
+    //iterate over each product in products
+    for (let i=0;i < products.length; i++) {
+      //options.uri = products[i];
+      options = {
+        method: 'GET',
+        uri: products[i],
+        gzip: true,
+        transform: function (body) {
+              return cheerio.load(body);
+          }
+      }
+      rp(options).then(
+        function($) {
+            var product = new Product(
+              $('#ebooksProductTitle').text().trim(),
+              $('.kindlePriceLabel').next().text().trim().split(' ')[0].replace('$', '')
+            )
+            console.log(product);
+          })
+  }
 });
